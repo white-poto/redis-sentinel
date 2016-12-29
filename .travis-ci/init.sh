6bc8fac4260285e8fcaa5e7b72c8726754c21ed3
huyanping <hypxm@qq.com>
2016-12-29 10:53:53 +0800

    tests

--- .travis-ci/init.sh
+++ .travis-ci/init.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-sh ./redis_install.sh
+sh .travis-ci/redis_install.sh
 mkdir -p /data/redis/6379
 mkdir -p /data/redis/6380
 mkdir -p /data/sentinel/26379
 mkdir -p /data/sentinel/26380
 
-redis-server etc/redis.6379.conf
-redis-server etc/redis.6380.conf
+redis-server .travis-ci/etc/redis.6379.conf
+redis-server .travis-ci/etc/redis.6380.conf
 
-redis-sentinel etc/sentinel.26379.conf
-redis-sentinel etc/sentinel.26380.conf
+redis-sentinel .travis-ci/etc/sentinel.26379.conf
+redis-sentinel .travis-ci/etc/sentinel.26380.conf
 
 
 ss -l
