5cff5f61906d43beb96c4e18d4d26c15ebe22bae
huyanping <hypxm@qq.com>
2016-12-29 14:14:12 +0800

    tests

--- tests/SentinelTest.php
+++ tests/SentinelTest.php
@@ -31,25 +31,28 @@ class SentinelTest extends \PHPUnit_Framework_TestCase
 
     public function testPing()
     {
-        $this->assertEquals("PONG", $this->sentinel->ping());
+        $this->assertEquals("+PONG", $this->sentinel->ping());
     }
 
     public function testMasters()
     {
         $masters = $this->sentinel->masters();
         $this->assertEquals(1, count($masters));
+        var_dump($masters);
         $this->assertEquals($this->master_name, $masters[0]['name']);
     }
 
     public function testMaster()
     {
         $master = $this->sentinel->master($this->master_name);
+        var_dump($master);
         $this->assertEquals($this->master_name, $master['name']);
     }
 
     public function testSlaves()
     {
         $slaves = $this->sentinel->slaves($this->master_name);
+        var_dump($slaves);
         $this->assertEquals(1, count($slaves));
         $this->assertEquals('127.0.0.1', $slaves[0]['ip']);
         $this->assertEquals('6380', $slaves[0]['port']);
@@ -58,6 +61,7 @@ class SentinelTest extends \PHPUnit_Framework_TestCase
     public function testSentinels($master_name)
     {
         $sentinels = $this->sentinel->sentinels($this->master_name);
+        var_dump($sentinels);
         $this->assertEquals(1, count($sentinels));
         $this->assertEquals('127.0.0.1', $sentinels[0]['ip']);
         $this->assertEquals(26380, $sentinels[0]['port']);
@@ -66,6 +70,7 @@ class SentinelTest extends \PHPUnit_Framework_TestCase
     public function testGetMasterAddrByName()
     {
         $address = $this->sentinel->getMasterAddrByName($this->master_name);
+        var_dump($address);
         $this->assertEquals('127.0.0.1', $address[0]);
         $this->assertEquals(6379, $address[1]);
     }
