4f2ac4810b012d8152a3b16426a490188d57ec4e
huyanping <hypxm@qq.com>
2017-01-03 15:28:52 +0800

    tests

--- .travis-ci/init.sh
+++ .travis-ci/init.sh
@@ -6,11 +6,13 @@ mkdir -p ./data/redis/6381
 mkdir -p ./data/sentinel/26379
 mkdir -p ./data/sentinel/26380
 mkdir -p ./data/sentinel/26381
+mkdir -p ./data/sentinel/26382
 
 
 ./redis/bin/redis-server .travis-ci/etc/redis.6379.conf
 ./redis/bin/redis-server .travis-ci/etc/redis.6380.conf
 ./redis/bin/redis-server .travis-ci/etc/redis.6381.conf
+./redis/bin/redis-server .travis-ci/etc/redis.6382.conf
 
 ./redis/bin/redis-sentinel .travis-ci/etc/sentinel.26379.conf
 ./redis/bin/redis-sentinel .travis-ci/etc/sentinel.26380.conf
